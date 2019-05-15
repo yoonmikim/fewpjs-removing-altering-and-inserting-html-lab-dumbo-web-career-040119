5e08f9afe00ed65874bb00b17a823675624a3867
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-15 18:33:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 // Write your code here!
-document.body.appendChild(element)
 
-var ul = document.createElement('ul')
- 
-for (let i = 0; i < 3; i++) {
-  let li = document.createElement('li')
-  li.innerHTML = (i + 1).toString()
-  ul.appendChild(li)
-}
- 
-element.appendChild(ul)
+const testVar = "hi"
 
-let element = document.querySelector("p#greeting");
-element.innerHTML = 'Hello, DOM!'
+/* Enter the code to remove the node element here */
+document.querySelector("main#main").remove();
+
+/* Create your new element here and assign it to newHeader */
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory";
+
 
 let header = document.getElementById("div#header");
 header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
