2d8f37badcb62fef66008e364f476af5dbe856e7
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-15 18:28:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,6 @@ element.appendChild(ul)
 
 let element = document.querySelector("p#greeting");
 element.innerHTML = 'Hello, DOM!'
+
+let header = document.getElementById("div#header");
+header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
