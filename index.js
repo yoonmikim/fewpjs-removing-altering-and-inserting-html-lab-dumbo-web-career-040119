ded005cfa068e0364cc90158440bfe5698e0a394
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-05-15 18:28:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 // Write your code here!
 document.body.appendChild(element)
+
+var ul = document.createElement('ul')
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+ 
+element.appendChild(ul)
